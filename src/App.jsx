63449462b5ab9d01fc31e9e9c46845d390e2a746
JustZeez <justzeez65@gmail.com>
2026-01-03 13:26:49 +0100

    fix: router import and tailwind config

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,9 @@ import { useState } from "react";
 import FloatingWhatsApp from "./componets/FloatingWhatsApp";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import Router from "./router/router";
+
 import "./App.css";
+import { Router } from "react-router-dom";
 
 function App() {
   return (
